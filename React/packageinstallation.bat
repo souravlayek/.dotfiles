9cc858e17e6a71fd7e5789f491ae736a75c708b6
souravlayek <souravlayek11@gmail.com>
2021-04-04 15:08:50 +0530

    react dot files

---
+++ React/packageinstallation.bat
@@ -0,0 +1,2 @@
+yarn add -D babel-eslint eslint eslint-config-prettier eslint-config-standard eslint-config-standard-react eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-node eslint-plugin-prettier eslint-plugin-promise eslint-plugin-react eslint-plugin-react-hooks prettier
+npx install-peerdeps --dev eslint-config-airbnb
